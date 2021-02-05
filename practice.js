eb09b55f89c8c6df5353c26bbec14a8952b3a82e
Gabriel <gabriel.jchen@gmail.com>
2021-02-05 00:23:11 +0000

    just a practice file

---
+++ practice.js
@@ -0,0 +1,13 @@
+let x = ""
+
+var laugh = function(num) {
+    
+    for(let i = 0 ; i < num ; i++){
+        
+        x = x + "ha"
+        
+    }
+    return x;
+    
+}
+console.log(laugh(3));
