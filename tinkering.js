6c7f37e6791242f706f3f254e00b3d47f8a3ab1b
Gabriel <gabriel.jchen@gmail.com>
2021-02-05 00:23:31 +0000

    another practice file not important

---
+++ tinkering.js
@@ -0,0 +1,23 @@
+let x = 60;
+
+while (x <= 60) {
+  if (x === 0) {
+      
+      console.log("Solid rocket booster ignition and liftoff!");
+      
+  } else if(x == 6) {
+      
+      console.log("Main engine start");
+      
+  } else if (x == 10) {
+      console.log("Activate main engine hydrogen burnoff system");
+      
+  } else if (x == 16) {
+      
+      console.log("Activate launch pad sound supression system");
+  } else if (x == 31) {
+      
+      console.log("Ground launch sequencer is go for auto sequence start");
+  }
+  
+  
